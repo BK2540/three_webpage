5d353974b44e863f58c097c397e5e88811dff6fe
Boongkee <124797798+BK2540@users.noreply.github.com>
2023-08-09 11:10:49 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { styled } from "styled-components"
+import Contact from "./component/Contact"
+import Hero from "./component/Hero"
+import Who from "./component/Who"
+import Works from "./component/Works"
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color:white;
+  background: url("./image/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
+function App() {
+
+  return (
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
+  )
+}
+
+export default App
